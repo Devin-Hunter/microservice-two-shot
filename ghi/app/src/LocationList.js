672d653ef2e6ca9485449430c46b93a709f670a4
noahD <noahharshb@gmail.com>
2024-01-31 15:12:14 -0700

    Added LocationList.js

---
+++ ghi/app/src/LocationList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function LocationsList({locations, getLocations}) {
+    if (locations === undefined) {
+        return null;
+    }
+
+    const handleDelete = async (location) => {
+        const locationUrl = `http://localhost:8100/api/locations/${location.id}/`;
+        const fetchConfig = {
+            method: 'delete'
+        }
+        const locationResponse = await fetch(locationUrl, fetchConfig);
+        if (locationResponse.ok) {
+            getLocations();
+        }
+    }
+
+    return (
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th>Closet Name</th>
+                    <th>Section Number</th>
+                    <th>Shelf Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                {locations.map(location => {
+                    console.log(location)
+                    return (
+                        <tr key={location.id}>
+                            <td>{ location.closet_name }</td>
+                            <td>{ location.section_number }</td>
+                            <td>{ location.shelf_number }</td>
+                            <td>
+                                <button type="button" id={location.id} onClick={() => handleDelete(location)} className="btn btn-danger">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
+
+export default LocationsList;
