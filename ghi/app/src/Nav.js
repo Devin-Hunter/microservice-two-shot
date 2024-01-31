ad7b9d4aea2ad1472a7747b2c10953e61d7188e6
noahD <noahharshb@gmail.com>
2024-01-31 13:10:25 -0700

    added Shoes nav to Nav.js

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,8 +1,8 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-info">
+    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#e3f2fd", marginBottom: "20px"}}>
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">Wardrobify</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,14 +11,21 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/shoes">Shoes</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/hats">Hats</NavLink>
-            </li>
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Hats
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/hats">Hats</Link></li>
+                    <li><Link className="dropdown-item" to="/hats/new">Create a New Hat</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           </ul>
         </div>
       </div>
